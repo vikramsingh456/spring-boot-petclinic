75b86033615f3724f19a3d86e4713731b7a9a79b
Vikram Singh <singh.vikram456@gmail.com>
2018-12-29 13:32:56 -0800

    Initial project add

---
+++ src/main/java/guru/springframework/springbootpetclinic/SpringBootPetclinicApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.springbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPetclinicApplication.class, args);
+	}
+
+}
+
